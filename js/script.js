1ef1f74f3b7dee60251e09dc524952f8ed9fe26f
Abhishek Kumar <abhi24033c@gmail.com>
2023-12-19 13:37:58 +0530

    "first commit"

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const hambergerIcon=document.getElementById("hambergerIcon");
+
+function toggleHamberger(){
+    hambergerIcon.classList.toggle('fa-xmark')
+}
+
+hambergerIcon.addEventListener("click",toggleHamberger);
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
