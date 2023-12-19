e16e9d0d106b6f4d99964d0e4288d5f3fdb63301
Abhishek Kumar <abhi24033c@gmail.com>
2023-12-19 13:39:12 +0530

    some comment removed

--- js/script.js
+++ js/script.js
@@ -6,25 +6,3 @@ function toggleHamberger(){
 
 hambergerIcon.addEventListener("click",toggleHamberger);
 
-
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
